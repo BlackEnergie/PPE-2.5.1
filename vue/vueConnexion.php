58e6be82703cf6669f5c6f24da476c02cdcea5d3
BlackEnergie <the.perrin@outlook.fr>
2017-09-08 14:57:09 +0200

    connexion

--- vue/vueConnexion.php
+++ vue/vueConnexion.php
@@ -3,13 +3,24 @@
 		<?php include 'haut.php' ;?>
 	</header>
 	<main>
-	<div class="">
+	<div class="connexion">
 		<form method="post" action="#">
 			<input type="text" placeholder="Identifiant" name="login" id="login" required="required">
 			<br>
 			<input type="password" placeholder="Mot de passe" name="password" id="password" required="required">
 			<br>
+			<input type="submit" value="Valider">
 		</form>
+		<?php
+			if(isset($_POST["password"])){
+				$password = $_POST["password"];
+			}
+
+			if(isset($_POST["login"])){
+				$login = $_POST["login"];
+			}
+
+		 ?>
 	</div>
 	</main>
 	<footer>
