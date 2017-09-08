8f59a94726ea8a7d16064166076e3b3b31301781
BlackEnergie <the.perrin@outlook.fr>
2017-09-08 15:34:25 +0200

    Connexion

--- vue/vueConnexion.php
+++ vue/vueConnexion.php
@@ -3,15 +3,22 @@
 		<?php include 'haut.php' ;?>
 	</header>
 	<main>
-	<div class="connexion">
-		<form method="post" action="#">
-			<input type="text" placeholder="Identifiant" name="login" id="login" required="required">
+	<div class="contentConnexion">
+		<form method="post" action="#" class="connexion">
 			<br>
-			<input type="password" placeholder="Mot de passe" name="password" id="password" required="required">
-			<br>
-			<input type="submit" value="Valider">
+				<legend class="titre">Connexion</legend>
+				<br>
+				<div class="corps">
+					<input type="text" placeholder="Identifiant" name="login" id="login" required="required">
+					<br><br>
+					<input type="password" placeholder="Mot de passe" name="password" id="password" required="required">
+					<br><br>
+					<input type="submit" value="Valider">
+				</div>
 		</form>
+
 		<?php
+
 			if(isset($_POST["password"])){
 				$password = $_POST["password"];
 			}
@@ -21,6 +28,7 @@
 			}
 
 		 ?>
+
 	</div>
 	</main>
 	<footer>
