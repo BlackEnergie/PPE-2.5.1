9e0b514ab1f4d4b776e6fc68ccc0f45cb406a1a2
BlackEnergie <the.perrin@outlook.fr>
2017-09-08 22:42:33 +0200

    Intervenant DTO

--- modele/dao.php
+++ modele/dao.php
@@ -80,6 +80,6 @@ class LigueDAO{
 		return $result;
 	}
 */
-}
+
 
 
--- modele/dto.php
+++ modele/dto.php
@@ -45,3 +45,70 @@ class Ligue {
     }
 }
 
+class Intervenant{
+    private $id;
+    private $nom;
+    private $prenom;
+    private $statut;
+    private $idClub;
+    private $login;
+    private $password;
+    private $idFonction;
+    private $codeRole;
+    private $idLigue;
+    
+    public function __construct($unId, $unNom, $unPrenom, $unStatut, $unIdClub, $unLogin, $unPwd, $uneFnctn, $unRole,$uneLigue){
+        $this->id = $unId;
+        $this->nom = $unNom;
+        $this->prenom = $unPrenom;
+        $this->statut = $unStatut;
+        $this->idClub = $unIdClub;
+        $this->login = $unLogin;
+        $this->password = $unPwd;
+        $this->idFonction = $uneFnctn;
+        $this->codeRole = $unRole;
+        $this->idLigue = $uneLigue;
+    }
+    
+    public function getId(){
+        return $this->id;
+    }
+    
+    public function getNom(){
+        return $this->nom;
+    }
+
+    
+    public function getPrenom(){
+        return $this->prenom;
+    }
+    
+    public function getStatut(){
+        return $this->statut;
+    }
+    
+    public function getIdClub(){
+        return $this->idClub;
+    }
+    
+    public function getLogin(){
+        return $this->login;
+    }
+    
+    public function getMdp(){
+        return $this->password;
+    }
+    
+    public function getFonction(){
+        return $this->idFonction;
+    }
+    
+    public function getRole(){
+        return $this->codeRole;
+    }
+    
+    public function getLigue(){
+        return $this->idLigue;
+    }
+    
+}
