6ddd6cf24e0e9c44a197c997b2d7f9eec250c3c0
ROBERT Thibault <robert.thibault33@gmail.com>
2017-09-15 16:08:05 +0200

    controleur commit

--- controleur/controleurBulletins.php
+++ controleur/controleurBulletins.php
@@ -21,30 +21,4 @@ else
 	}
 }
 
-
-/*****************************************************************************************************
- * Récupérer le bulletin sélectionnée
- *****************************************************************************************************/
-
-/*  $equipeActive = $_SESSION['lesBulletins']->chercheBulletin($_SESSION['bulletin']);
-
-if($_SESSION['bulletin'] != 0 && isset($_POST['modifier'])){
-    $bulletinActive->setNomEquipe($_POST['nomEquipe']);
-    $bulletinActive->setNomEquipeLong($_POST['nomEquipeLong']);
-    $bulletinActive->setNomEntraineur($_POST['nomEntraineur']);
-<<<<<<< HEAD
-   $bulletinActive->setNomPresident($_POST['nomPresident']);
-   $bulletinActive->setDateFondation($_POST['dateFondation']);
-} */
-
-=======
-    $bulletinActive->setNomPresident($_POST['nomPresident']);
-    $bulletinActive->setDateFondation($_POST['dateFondation']);
-<<<<<<< HEAD
-    BulletinDAO::modifier($equipeActive);
-=======
->>>>>>> c3d7e11ee3d1c87b12ace5b360ddeed30e110e69
-}
->>>>>>> 7e1dfd3aa3d09f8c3bdb9fa56c0ae74e6d86e6b3
-
 require_once 'vue/vueBulletins.php' ;
