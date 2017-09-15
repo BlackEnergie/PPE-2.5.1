531bd735c04179dc0c9ae6c0ee441ad7b0fd6125
Mika Gruber <migra.gruber@gmail.com>
2017-09-15 13:28:52 +0200

    vue bas

--- vue/bas.php
+++ vue/bas.php
@@ -1,5 +1,5 @@
 <p>
-Gestion de ligues et des clubs réalisée par : <br/>
-Gestion des intervenants et des bulletins de salaire réalisée par : <br/>
-Gestion des formations réalisée par :<br/>
+Gestion de ligues et des clubs réalisée par : GRUBER Mika <br/>
+Gestion des intervenants et des bulletins de salaire réalisée par : ROBERT Thibault<br/>
+Gestion des formations réalisée par : PERRIN Théo<br/>
 </p>
