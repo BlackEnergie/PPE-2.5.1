f4a715fc7367f435f1cb65fbfff4390297ca1197
ROBERT Thibault <robert.thibault33@gmail.com>
2017-09-15 13:13:17 +0200

    dao

--- modele/dao.php
+++ modele/dao.php
@@ -2,14 +2,14 @@
 class DBConnex extends PDO{
 
 	private static $instance;
-	
+
 	public static function getInstance(){
 		if ( !self::$instance ){
 			self::$instance = new DBConnex();
 		}
 		return self::$instance;
 	}
-		
+
 	function __construct(){
 		try {
 			parent::__construct($dsn ,$user, $pass);
@@ -18,55 +18,95 @@ class DBConnex extends PDO{
 			die("Impossible de se connecter. " );
 		}
 	}
-	
-		
-	
-	
+
+
+
+
 	public function queryFetchAll($sql){
 		$sth  = $this -> query($sql);
-	
+
 		if ( $sth ){
 			return $sth -> fetchAll(PDO::FETCH_ASSOC);
 		}
-	
+
 		return false;
 	}
-		
-	
+
+
 	public function queryFetchFirstRow($sql){
 		$sth    = $this -> query($sql);
 		$result    = array();
-	
+
 		if ( $sth ){
 			$result  = $sth -> fetch();
 		}
-	
+
 		return $result;
 	}
-	
-	
+
+
 }
-	
+
 class LigueDAO{
-    
+
     public function lire(Ligue $ligue){
         $sql = "select * from ligue where IDL=" . $ligue->getidLigue();
         $ligue = DBConnex::getInstance()->queryFetchFirstRow($sql);
         return $ligue;
     }
-    
+
+}
+
+class IntervenantDAO{
+
+	public function lire(Intervenant $intervenant)
+		$sql = "select * from intervenant where IDL=" . $intervenant->getidInvenant();
+		$intervenant = DBConnex::getInstance()->queryFetchFirstRow($sql);
+		return $intervenant;
+}
+
+class ClubDAO{
+
+	public function lire(Club $club)
+		$sql = "select * from club where IDL=" . $club->getidInvenant();
+		$club = DBConnex::getInstance()->queryFetchFirstRow($sql);
+		return $club;
+}
+
+class ContratDAO{
+
+	public function lire(Contrat $contrat)
+		$sql = "select * from contrat where IDL=" . $contrat->getidInvenant();
+		$contrat = DBConnex::getInstance()->queryFetchFirstRow($sql);
+		return $contrat;
+}
+
+class BulletinDAO{
+
+	public function lire(Bulletin $bulletin)
+		$sql = "select * from bulletin where IDL=" . $bulletin->getidInvenant();
+		$bulletin = DBConnex::getInstance()->queryFetchFirstRow($sql);
+		return $bulletin;
+}
+
+class FormationDAO{
+
+	public function lire(Formation $formation)
+		$sql = "select * from Formation where IDL=" . $formation->getidInvenant();
+		$formation = DBConnex::getInstance()->queryFetchFirstRow($sql);
+		return $formation;
 }
 
 /* class EquipeDAO{
-		
+
 	public function lire(Equipe $equipe){
 		$sql = "select * from Equipe where idEquipe = " . $equipe->getIdEquipe();
 		$equipe = DBConnex::getInstance()->queryFetchFirstRow($sql);
 		return $equipe;
 	}
 
-	
-	
+
+
 	public static function lesEquipes(){
 		$result = array();
 		$sql = "select * from equipe order by nomEquipe" ;
@@ -80,6 +120,3 @@ class LigueDAO{
 		return $result;
 	}
 */
-
-
-
