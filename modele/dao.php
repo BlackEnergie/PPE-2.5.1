ff96a6a4ad24fa6ff7dd3a868cad74677580419d
ROBERT Thibault <robert.thibault33@gmail.com>
2017-09-15 15:18:00 +0200

    dto dao

--- modele/dao.php
+++ modele/dao.php
@@ -47,6 +47,7 @@ class DBConnex extends PDO{
 
 }
 
+/////////////////////////////
 class LigueDAO{
 
     public function lire(Ligue $ligue){
@@ -57,49 +58,126 @@ class LigueDAO{
 
 }
 
+public static function lesLigues(){
+	$result = array();
+	$sql = "select * from ligue order by NOMLIGUE" ;
+	$liste = DBConnex::getInstance()->queryFetchAll($sql);
+	if(count($liste)> 0){
+		foreach($liste as $ligue){
+			$uneLigue = new Ligue($ligue[0],$ligue[1], $ligue[2],  $ligue[3]);
+			$result[] = $uneEquipe;
+		}
+	}
+	return $result;
+}
+
+////////////////////////////////////////////////
 class IntervenantDAO{
 
-	public function lire(Intervenant $intervenant){
+	public function lire(Intervenant $intervenant)
 		$sql = "select * from intervenant where IDL=" . $intervenant->getidInvenant();
 		$intervenant = DBConnex::getInstance()->queryFetchFirstRow($sql);
 		return $intervenant;
-	}
 }
 
+public static function lesIntervenants(){
+	$result = array();
+	$sql = "select * from intervenant order by nomEquipe" ;
+	$liste = DBConnex::getInstance()->queryFetchAll($sql);
+	if(count($liste)> 0){
+		foreach($liste as $intervenant){
+			$unIntervenant = new Intervenant($intervenant[0],$intervenant[1], $intervenant[2],  $intervenant[3],  $intervenant[4],  $intervenant[5],  $intervenant[6],  $intervenant[7],  $intervenant[8],  $intervenant[9] );
+			$result[] = $unIntervenant;
+		}
+	}
+	return $result;
+}
+/////////////////////////////////////////
 class ClubDAO{
 
-	public function lire(Club $club){
+	public function lire(Club $club)
 		$sql = "select * from club where IDL=" . $club->getidInvenant();
 		$club = DBConnex::getInstance()->queryFetchFirstRow($sql);
 		return $club;
-	}
 }
 
+public static function lesClub(){
+	$result = array();
+	$sql = "select IDCL, NOMCLUB, ADRESSECLUB from club order by NOMCLUB" ;
+	$liste = DBConnex::getInstance()->queryFetchAll($sql);
+	if(count($liste)> 0){
+		foreach($liste as $club){
+			$unClub = new Club($club[0],$club[1], $club[2]);
+			$result[] = $unClub;
+		}
+	}
+	return $result;
+}
+
+////////////////////////////////////////////
 class ContratDAO{
 
-	public function lire(Contrat $contrat){
+	public function lire(Contrat $contrat)
 		$sql = "select * from contrat where IDL=" . $contrat->getidInvenant();
 		$contrat = DBConnex::getInstance()->queryFetchFirstRow($sql);
 		return $contrat;
-	}
 }
 
+public static function lesContrats(){
+	$result = array();
+	$sql = "select ID, DATEDEBUT, DATEFIN, TYPECONTRAT, NHEURES from contrat order by ID" ;
+	$liste = DBConnex::getInstance()->queryFetchAll($sql);
+	if(count($liste)> 0){
+		foreach($liste as $contrat){
+			$unContrat = new Contrat($contrat[0],$contrat[1], $contrat[2], $contrat[3], $contrat[4]);
+			$result[] = $unContrat;
+		}
+	}
+	return $result;
+}
+
+//////////////////////////////////////////////////
 class BulletinDAO{
 
-	public function lire(Bulletin $bulletin){
+	public function lire(Bulletin $bulletin)
 		$sql = "select * from bulletin where IDL=" . $bulletin->getidInvenant();
 		$bulletin = DBConnex::getInstance()->queryFetchFirstRow($sql);
 		return $bulletin;
-	}
 }
 
+public static function lesBulletins(){
+	$result = array();
+	$sql = "select IDB, MOIS, ANNEE, BULLETININPDF from bulletin order by IDB" ;
+	$liste = DBConnex::getInstance()->queryFetchAll($sql);
+	if(count($liste)> 0){
+		foreach($liste as $contrat){
+			$unBulletin = new Bulletin($bulletin[0],$bulletin[1], $bulletin[2], $bulletin[3]);
+			$result[] = $unBulletin;
+		}
+	}
+	return $result;
+}
+
+///////////////////////////////////////////////////////////
 class FormationDAO{
 
-	public function lire(Formation $formation){
+	public function lire(Formation $formation)
 		$sql = "select * from Formation where IDL=" . $formation->getidInvenant();
 		$formation = DBConnex::getInstance()->queryFetchFirstRow($sql);
 		return $formation;
+}
+
+public static function lesFormations(){
+	$result = array();
+	$sql = "select * from formation order by nomEquipe" ;
+	$liste = DBConnex::getInstance()->queryFetchAll($sql);
+	if(count($liste)> 0){
+		foreach($liste as $equipe){
+			$uneEquipe = new Equipe($equipe['idEquipe'],$equipe['nomEquipe'], $equipe['nomEquipeLong'],  $equipe['nomEntraineur'],  $equipe['nomPresident'],  $equipe['dateFondation'] );
+			$result[] = $uneEquipe;
+		}
 	}
+	return $result;
 }
 
 /* class EquipeDAO{
--- modele/dto.php
+++ modele/dto.php
@@ -1,45 +1,45 @@
-<?php 
+<?php
 class Ligue {
     private $id;
     private $nom;
     private $site;
     private $description;
-    
+
     public function __construct($unId, $unNom, $unSite, $unDescriptif){
         $this->id = $unId;
         $this->nom = $unNom;
         $this->site = $unSite;
         $this->description = $unDescriptif;
     }
-    
+
     public function getId(){
         return $this->id;
     }
-    
+
     public function setId($unId){
         $this->id = $unId;
     }
-        
+
     public function getNom(){
         return $this->nom;
     }
-    
+
     public function setNom($unNom){
         $this->nom = $unNom;
     }
-    
+
     public function getSite(){
         return $this->site;
     }
-    
+
     public function setSite($unSite){
         $this->site = $unSite;
     }
-    
+
     public function getDescription(){
         return $this->description;
     }
-    
+
     public function setDescription($uneDescription){
         $this->description = $uneDescription;
     }
@@ -47,16 +47,16 @@ class Ligue {
 
 class Intervenant{
     private $id;
+    private $idFonction;
+    private $idClub;
+    private $idLigue;
+    private $codeRole;
     private $nom;
     private $prenom;
     private $statut;
-    private $idClub;
     private $login;
     private $password;
-    private $idFonction;
-    private $codeRole;
-    private $idLigue;
-    
+
     public function __construct($unId, $unNom, $unPrenom, $unStatut, $unIdClub, $unLogin, $unPwd, $uneFnctn, $unRole,$uneLigue){
         $this->id = $unId;
         $this->nom = $unNom;
@@ -69,48 +69,48 @@ class Intervenant{
         $this->codeRole = $unRole;
         $this->idLigue = $uneLigue;
     }
-    
+
     public function getId(){
         return $this->id;
     }
-    
+
     public function getNom(){
         return $this->nom;
     }
 
-    
+
     public function getPrenom(){
         return $this->prenom;
     }
-    
+
     public function getStatut(){
         return $this->statut;
     }
-    
+
     public function getIdClub(){
         return $this->idClub;
     }
-    
+
     public function getLogin(){
         return $this->login;
     }
-    
+
     public function getMdp(){
         return $this->password;
     }
-    
+
     public function getFonction(){
         return $this->idFonction;
     }
-    
+
     public function getRole(){
         return $this->codeRole;
     }
-    
+
     public function getLigue(){
         return $this->idLigue;
     }
-    
+
 }
 
 class Club {
@@ -119,7 +119,7 @@ class Club {
     private $codeVille;
     private $nomClub;
     private $adresse;
-    
+
     public function __construct($unId, $uneLigue, $unCodeVille, $unNomClub, $uneAdresse){
         $this->id = $unId;
         $this->ligue = $uneLigue;
@@ -127,27 +127,27 @@ class Club {
         $this->nomClub = $unNomClub;
         $this->adresse = $uneAdresse;
     }
-    
+
     public function getId(){
         return $this->id;
     }
-    
+
     public function getLigue(){
         return $this->ligue;
     }
-    
+
     public function getCodeVille(){
         return $this->codeVille;
     }
-    
+
     public function getNomClub(){
         return $this->nomClub;
     }
-    
+
     public function getAdresse(){
         return $this->adresse;
     }
-    
+
 }
 
 class Contrat {
@@ -156,7 +156,7 @@ class Contrat {
     private $dateFin;
     private $typeContrat;
     private $nbHeures;
-    
+
     public function __construct($id, $debut, $fin, $type, $heures){
         $this->id = $id;
         $this->dateDebut = $debut;
@@ -164,27 +164,27 @@ class Contrat {
         $this->typeContrat = $type;
         $this->nbHeures = $heures;
     }
-    
+
     public function getId(){
         return $this->id;
     }
-    
+
     public function getDateDebut(){
         return $this->dateDebut;
     }
-    
+
     public function getDateFin(){
         return $this->dateFin;
     }
-    
+
     public function getTypeContrat(){
         return $this->typeContrat;
     }
-    
+
     public function getNbHeures(){
         return $this->nbHeures;
     }
-    
+
 }
 
 class Bulletin {
@@ -192,26 +192,26 @@ class Bulletin {
     private $mois;
     private $annee;
     private $pdf;
-    
+
     public function __construct($numero, $mois, $annee, $pdf){
         $this->numero = $numero;
         $this->mois = $mois;
         $this->annee = $annee;
         $this->pdf = $pdf;
     }
-    
+
     public function getNumero(){
         return $this->numero;
     }
-    
+
     public function getMois(){
         return $this->mois;
     }
-    
+
     public function getAnnee(){
         return $this->annee;
     }
-    
+
     public function getPDF(){
         return $this->pdf;
     }
@@ -227,7 +227,7 @@ class Formation {
     private $clotureInscription;
     private $duree;
     private $effectifMax;
-    
+
     public function __construct($id, $titre, $description, $dateDebut, $dateFin, $Oinscription, $Finscription, $duree, $effectif){
         $this->id = $id;
         $this->titre = $titre;
@@ -239,39 +239,39 @@ class Formation {
         $this->duree = $duree;
         $this->effectifMax = $effectif;
     }
-    
+
     public function getId(){
         return $this->id;
     }
-    
+
     public function getTitre(){
         return $this->titre;
     }
-    
+
     public function getDescription(){
         return $this->description;
     }
-    
+
     public function getDateDebut(){
         return $this->dateDebut;
     }
-    
+
     public function getDateFin(){
         return $this->dateFin;
     }
-    
+
     public function getDateOuvertureInscri(){
         return $this->ouvertureInscriptions;
     }
-    
+
     public function getDateFermetureInscri(){
         return $this->clotureInscription;
     }
-    
+
     public function getDuree(){
         return $this->duree;
     }
-    
+
     public function getEffectifMax(){
         return $this->effectifMax;
     }
