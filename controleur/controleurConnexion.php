c30569c3d8c7817f86d08fa06be65922cdba24a7
robertthibault <31767944+robertthibault@users.noreply.github.com>
2017-09-22 16:20:55 +0200

    controleur connexion

--- controleur/controleurConnexion.php
+++ controleur/controleurConnexion.php
@@ -1,31 +1,29 @@
 <?php
 
-if(!isset($_SESSION['identification']) || !$_SESSION['identification']){
-    
-    
-    $formulaireConnexion = new Formulaire('post', 'index.php', 'fConnexion', 'fConnexion');
-    $formulaireConnexion->ajouterComposantLigne($formulaireConnexion->creerLabelFor('identifiant', 'Identifiant :'), 1);
-    $formulaireConnexion->ajouterComposantLigne($formulaireConnexion->creerInputTexte('login', 'login', ''   , 1, '',0),1);
-    $formulaireConnexion->ajouterComposantTab();
-    
-    $formulaireConnexion->ajouterComposantLigne($formulaireConnexion->creerLabelFor('mdp', 'Mot de passe :'), 1);
-    $formulaireConnexion->ajouterComposantLigne($formulaireConnexion->creerInputPass('mdp', 'mdp', '' ,1),1);
-    $formulaireConnexion->ajouterComposantTab();
-    
-    $formulaireConnexion->ajouterComposantLigne($formulaireConnexion-> creerInputSubmit('submitConnex', 'submitConnex', 'Valider'),2);
-    $formulaireConnexion->ajouterComposantTab();
-    
-    $formulaireConnexion->ajouterComposantLigne($formulaireConnexion->creerLabel($messageErreurConnexion, "messageErreurConnexion"),2);
-    $formulaireConnexion->ajouterComposantTab();
-    
-    $formulaireConnexion->creerFormulaire();
-    
-    include_once 'vues/squeletteConnexion.php';
-    
-}
-else{
-    $_SESSION['identification']=arrey();
-    $_SESSION['menuPrincipalC']="equipeC";
-    header('location: index.php');
-    
-}
+$interfaceConnexion = new Formulaire("GET", "index.php", "InterfaceConnexion","connexion");
+
+
+$titre="<label class='titre'> Connexion </label>";
+$interfaceConnexion->ajouterComposantLigne($titre);
+$interfaceConnexion->ajouterComposantTab();
+
+$label=$interfaceConnexion->creerLabel("Login : ");
+$composant=$interfaceConnexion->creerInputTexte("login", "login", "", 1, "Login", "");
+$composant=$interfaceConnexion->concactComposants($label, $composant);
+$interfaceConnexion->ajouterComposantLigne($composant);
+
+$label=$interfaceConnexion->creerLabel("Mot de passe : ");
+$composant=$interfaceConnexion->creerInputMdp("pw", "pw", 1, "Mot de passe", "");
+$composant=$interfaceConnexion->concactComposants($label, $composant);
+$interfaceConnexion->ajouterComposantLigne($composant);
+$interfaceConnexion->ajouterComposantTab();
+
+$composant=$interfaceConnexion->creerInputSubmit("submit", "submit", "Connexion");
+$interfaceConnexion->ajouterComposantLigne($composant);
+$interfaceConnexion->ajouterComposantTab();
+
+$interfaceConnexion->creerFormulaire();
+
+
+
+require_once 'vue/vueConnexion.php' ;
